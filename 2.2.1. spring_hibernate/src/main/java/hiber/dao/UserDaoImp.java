fe5f5a313e5147bc197ea7faf7102af822552ccd
asbankov <ant.s.bankov@gmail.com>
2022-07-02 03:47:07 +0300

    hibernateSpring v 1.0

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,10 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -20,10 +23,33 @@ public class UserDaoImp implements UserDao {
    }
 
    @Override
+   public void addWithCar(User user, Car car) {
+      Session session = sessionFactory.getCurrentSession();
+      user.setCar(car);
+      car.setUser(user);
+      session.save(user);
+      session.save(car);
+   }
+
+   @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   public User userFromCar (String model, int series) {
+      String hql = "from User as u where u.car.model = :model and u.car.series = :series";
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery(hql);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.getSingleResult();
+   }
+
+   public void removeUserWithCar(User user) {
+      Session session = sessionFactory.getCurrentSession();
+      session.delete(user);
+      session.delete(user.getCar());
    }
 
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,6 +1,7 @@
 package hiber.service;
 
 import hiber.dao.UserDao;
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,4 +27,19 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional
+   @Override
+   public void addWithCar(User user, Car car) {userDao.addWithCar(user, car);}
+
+   @Transactional
+   @Override
+   public User userFromCar(String model, int series) {
+      return userDao.userFromCar(model, series);
+   }
+
+   @Transactional
+   @Override
+   public void removeUserWithCar(User user) {
+      userDao.removeUserWithCar(user);
+   }
 }
